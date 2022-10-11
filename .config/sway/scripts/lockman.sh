31a797538ce9ccccabc910cb829908c15230dc1a
Arya K <73596856+gi-yt@users.noreply.github.com>
2022-10-11 13:21:51 +0530

    Codeberg

--- .config/sway/scripts/lockman.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Times the screen off and puts it to background
-swayidle \
-    timeout 300 'swaymsg "output * dpms off"' \
-    resume 'swaymsg "output * dpms on"' &
-# Locks the screen immediately
-swaylock --screenshots --clock --indicator --indicator-radius 100 --indicator-thickness 7 --effect-blur 7x5 --effect-vignette 0.5:0.5 --ring-color bb00cc --key-hl-color 880033 --line-color 00000000 --inside-color 00000088 --separator-color 00000000 --fade-in 0.2
-# Kills last background task so idle timer doesn't keep running
-kill %%
--- .config/sway/scripts/wallpaper.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-killall swaybg
-while true; do
-	kill $PID
-       	wallpaper=$(find "$HOME/Pictures/Wallpapers" -type f | shuf -n 1)
-       	swaybg -i "$wallpaper" &
-	PID=$!
-       	sleep 10s; done
